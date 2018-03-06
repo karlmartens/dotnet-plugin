997659280ab108d9c51632089057cabca55de517
Karl Martens <karl.martens@3esi-enersight.com>
2018-03-06 11:44:28 -0700

    Add tasks with common names "compile", "build", "publish" to support easier multi-project builds.

--- buildSrc/src/main/java/net/karlmartens/dotnet/DotnetPlugin.java
+++ buildSrc/src/main/java/net/karlmartens/dotnet/DotnetPlugin.java
@@ -2,6 +2,7 @@ package net.karlmartens.dotnet;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.TaskContainer;
 
 import java.util.Arrays;
 
@@ -19,32 +20,43 @@ public class DotnetPlugin implements Plugin<Project> {
     public void apply(Project project) {
         final DotnetExtension extension = project.getExtensions().create("dotnet", DotnetExtension.class);
 
-        project.getTasks().create(INFO_TASK, Info.class, task -> {
+        TaskContainer tasks = project.getTasks();
+        tasks.create(INFO_TASK, Info.class, task -> {
             task.setExtension(extension);
         });
-        project.getTasks().create(CLEAN_TASK, Clean.class, task -> {
-            task.setExtension(extension);
-            task.setDependsOn(Arrays.asList(INFO_TASK));
-        });
-        project.getTasks().create(RESTORE_TASK, Restore.class, task -> {
+        tasks.create(CLEAN_TASK, Clean.class, task -> {
             task.setExtension(extension);
             task.setDependsOn(Arrays.asList(INFO_TASK));
         });
-        project.getTasks().create(COMPILE_TASK, Compile.class, task -> {
+        tasks.create(RESTORE_TASK, Restore.class, task -> {
+            task.setExtension(extension);
+            task.setDependsOn(Arrays.asList(INFO_TASK));
+        });
+        tasks.create(COMPILE_TASK, Compile.class, task -> {
             task.setExtension(extension);
             task.setDependsOn(Arrays.asList(CLEAN_TASK, RESTORE_TASK));
         });
-        project.getTasks().create(TEST_TASK, Test.class, task -> {
+        tasks.create(TEST_TASK, Test.class, task -> {
             task.setExtension(extension);
             task.setDependsOn(Arrays.asList(COMPILE_TASK));
         });
-        project.getTasks().create(DOCS_TASK, Docs.class, task -> {
+        tasks.create(DOCS_TASK, Docs.class, task -> {
             task.setExtension(extension);
             task.setDependsOn(Arrays.asList(COMPILE_TASK));
         });
-        project.getTasks().create(PUBLISH_TASK, Publish.class, task -> {
+        tasks.create(PUBLISH_TASK, Publish.class, task -> {
             task.setExtension(extension);
             task.setDependsOn(Arrays.asList(TEST_TASK));
+        });
+
+        tasks.create("clean", task -> {
+           task.setDependsOn(Arrays.asList(CLEAN_TASK));
+        });
+        tasks.create("build", task -> {
+            task.setDependsOn(Arrays.asList(COMPILE_TASK));
+        });
+        tasks.create("publish", task -> {
+            task.setDependsOn(Arrays.asList(PUBLISH_TASK));
         });
     }
 }
